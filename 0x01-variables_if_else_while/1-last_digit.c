540656c8fe362afbe150ec2836658d56985f99df
Rawy450 <Rawy450>
2023-08-14 14:25:58 -0700

    my first commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,14 +1,29 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+/**
+ * main - entry point
+ *
+ * Desscription: print the value
 
-/* betty style doc for function main goes there */
+ * Return: 0 (success0)
+
+*/
+
 int main(void)
+
 {
-	int n;
+	int n; digit
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
+	digit = n % 10;
+	if (digit > 5)
+		printf("last digit of %i is %i and is greater than 5\n", n, digit);
+	else if (digit == 0)
+		 printf("last digit of %i is %i and is 0\n", n, digit);
+	else if
+		(digit < 6 && digit != 0)
+		 printf("last digit of %i is %i and is less than 6 not 0\n", n, digit);
+
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: print all aplhabet lstters
+ *
+ * Return: always 0 (success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'Z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}

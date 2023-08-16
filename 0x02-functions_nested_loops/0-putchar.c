706a22c391b60f06451625f8d788f99b39e70a50
Rawy450 <Rawy450>
2023-08-16 09:38:39 -0700

    my first commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,12 @@
 #include"main.h"
 /**
- * main - entry point
- *
- * Description: print _ putchar
- * Return: always 0 (success)
-*/
+  * main - entry point
+  *
+  * Description: print _ putchar
+  * Return: always 0 (success)
+  */
 
 int main(void)
-
 {
 	char str[] = "_putchar";
 	int ch;

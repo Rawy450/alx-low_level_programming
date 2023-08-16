7f86882f41678220b1ba09880acc98b4ddb2289a
Rawy450 <Rawy450>
2023-08-16 10:25:21 -0700

    my first commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10.c
+ *
+ * Return: Always 0.
+ */
+int void print_alphabet_x10(void)
+
+{
+	int line, ch;
+
+		for (line = 0; line < 10; line++)
+		{
+		for (ch = 'a'; ch <= 'z'; ch++)
+			_putchar(ch);
+		_putchar('\n');
+		}
+	return (0);
+}

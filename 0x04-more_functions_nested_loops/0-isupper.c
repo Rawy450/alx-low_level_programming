d028e3d7d89bf6ac766e73613946fba714563e34
Rawy450 <Rawy450>
2023-08-17 11:32:14 -0700

    my first commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - if c is upper
+ *@c: input for alphabet
+ * Return: 1 if it upper
+ */
+int _isupper(int c)
+{
+	char c;
+
+	if (c >= 65; c <= 90)
+
+		return (1);
+	else
+		return (0);
+}

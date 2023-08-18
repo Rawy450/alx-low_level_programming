1e34713863db180e69023537f3493d476a5641a6
Rawy450 <Rawy450>
2023-08-17 18:50:58 -0700

    my first commit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,11 +11,13 @@ void more_numbers(void)
 	for (row == 1; row <= 10; row++)
 	{
 		for (count == 0; count <= 14; count++)
-			num = count
+		{	num = count
 				if (count > 9)
-					_putchar(1 + 48);
+				{	_putchar(1 + 48);
 			num = count % 10;
-	}
+				}
+				_putchar(num + 48);
+		}
 		_putchat('\n');
 	}
 }

108975234ead132d15b6e508e99299f30bd91097
Rawy450 <Rawy450>
2023-08-17 18:22:31 -0700

    my first commit

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+  * main - check the code
+  * Return: Always 0.
+*/
+
+void more_numbers(void)
+{
+	int num, row, count
+
+	for (row == 0; row <= 10; row++)
+	{
+		{
+			for (count == 0; count <= 14; count++)
+			num = count
+				if (count > 9)
+					_putchar(1 + 48);
+			num = count % 10;
+	}
+		_putchat('\n');
+	}
+}
+
+

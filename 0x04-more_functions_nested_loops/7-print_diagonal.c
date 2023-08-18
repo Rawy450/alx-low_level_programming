e4759feaa937c07ce44e8afd665e356d0a0b18e8
Rawy450 <Rawy450>
2023-08-18 02:32:06 -0700

    my first commit

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+  * print_diagonal - print a diagonal line on the terminal
+  * @n: input of number of times
+*/
+
+void print_diagonal(int n)
+{
+	int postn, space;
+
+	if (n <= 0)
+	_putchar('\n');
+	else
+	{
+		for (postn = 1; postn <= n; postn++)
+		{
+			for (space = 1; space <= postn; space++)
+				_putchar(' ');
+			_putchar('92');/* equal to '/' char*/
+		_putchar('\n');
+		}
+	}
+}

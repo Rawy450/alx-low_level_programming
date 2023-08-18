21bbee1955e3640e142e291ad8c9035a5b420f7e
Rawy450 <Rawy450>
2023-08-17 17:03:15 -0700

    my first commit

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,11 +1,10 @@
 #include "main.h"
 /**
-  * mul - multiplies two integers
-  * @a: first input
-  * @b: second input
-  *
-  * Ruturn: return results
-*/
+ * mul - checks for checks for a digit (0 through 9).
+ * @a: a - Variable
+ * @b: b - variable
+ * Return: Always 0.
+ */
 
 int mul(int a, int b)
 {

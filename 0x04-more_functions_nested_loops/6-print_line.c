ad7a29531a1fb1f0a96409ab5c7d6be7e5cf6328
Rawy450 <Rawy450>
2023-08-18 01:41:38 -0700

    my first commit

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,14 +7,14 @@
 
 void print_line(int n)
 {
-	int lnchr:
+	int lnChr;
 
 		if (n <= 0)
 			_putchar('\n');
 		else
 		{
-			for (lnchr = 1; lnchr <= n; lnchr++)
-				  _putchar('_');
+			for (lnChr = 1; lnChr <= n; lnChr++)
+				 _putchar('_');
 			  _putchar('\n');
 		}
 }

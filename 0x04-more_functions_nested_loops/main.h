1e4d534d9972ce67c02d3cce22833a21ca09cbcd
Rawy450 <Rawy450>
2023-08-17 23:05:16 -0700

    my first commit

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,11 +2,6 @@
 #define _MAIN_H_
 
 int _putchar(char c);
-int add(int a, int b);
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
-void print_numbers(void);
 void more_numbers(void);
 
 #endif

774b785996c3e616beb21425257e05912a14cb68
Rawy450 <Rawy450>
2023-08-18 01:18:25 -0700

    my first commit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,12 +7,12 @@
 
 void print_numbers(void)
 {
-	int num = 0
+	int num = 0;
 
 	do {
 		_putchar(num + 48);
 		num++;
 	}
-	while (i >= 0 && i <= 9)
+	while (num >= 0 && num <= 9);
 		_putchar('\n');
 }

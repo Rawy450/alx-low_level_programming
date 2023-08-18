2b2b1c1265bb40b439c06095c3d2b5dd7cd277b5
Rawy450 <Rawy450>
2023-08-17 17:32:44 -0700

    my first commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  * print_numbers - print  from 0 to 9
+  *	only use _putchar twice in your code
+  *
+  * Return: 0 (success)
+*/
+
+void print_numbers(void)
+{
+	int i = 0
+
+	do {
+		_putchar(i + 48);
+		i++;
+	} while (i >= 0 && i <= 9);
+		_putchar ('\n');
+}

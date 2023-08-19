83bce67f9d7cae1de0f793d7d275a8932a6cb811
Rawy450 <Rawy450>
2023-08-19 12:26:00 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,17 +10,12 @@
 int main(void)
 {
 	char ch = 'a';
-	char CH = 'A';
 	/*print a-z*/
 
 	while (ch <= 'z')
 	{
 		putchar(ch);
 		ch++;
-	}
-	while (CH <= 'Z')
-	{		putchar(CH);
-		CH++;
 	}
 	putchar('\n');
 	return (0);

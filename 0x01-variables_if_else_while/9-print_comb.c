1a117c16eb816ded15aad36cdb75d937345165a4
Rawy450 <Rawy450>
2023-08-20 09:40:07 -0700

    a

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+/**
+* main - entry point
+* Desciption: print 0, 1, -9
+* return: always (0)
+*/
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		putchar('digit + 48');
+		if (digit != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		digit++;
+	}
+	putchar('\n');
+	return (0);
+}

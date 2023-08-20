3d970d3eebc387304b428d12d4fef73e55a96013
Rawy450 <Rawy450>
2023-08-20 10:30:59 -0700

    a

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+/**
+  * main - entry point
+  *
+  * Description: prints all possible different combinations of two digits
+  *
+  * Return: always 0 succes
+*/
+int main(void)
+{
+	int d1 = 0, d2;
+
+	while (d1 <= 9)
+		d1 = 0;
+	while (d2 <= 9)
+	{
+		if (d1 != d2 && d1 < d2)
+		{
+			putchar(d1 + 48);
+			purchat(d2 + 48);
+			if (d1 + d2 != 17)
+				putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+		return (0);
+}

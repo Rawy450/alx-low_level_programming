6ff174571a24548169967d515b968bf731d81047
Rawy450 <Rawy450>
2023-08-21 10:40:56 -0700

    s

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,6 +4,7 @@
   * void reset_to_98 - function of  pointer to an int as parameter and
   *		updates the value it points to to 98
   * @n: input
+  * Return: always 0 success
   */
 
 void reset_to_98(int *n)

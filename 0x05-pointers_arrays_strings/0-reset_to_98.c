d55d6fc318533c92b9a7ed364c8cdcae137c517c
Rawy450 <Rawy450>
2023-08-21 09:07:08 -0700

    s

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  * void reset_to_98 - function of  pointer to an int as parameter and
+  *		updates the value it points to to 98
+  * @n: input
+  */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int add(int a, int b);
+void reset_to_98(int *n);
+#endif

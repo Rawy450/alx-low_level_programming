04e1d8c3948b8f9aaa1e9680e368b85dde3970ad
Rawy450 <Rawy450>
2023-08-24 10:55:22 -0700

    s

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include<main.h>
+/**
+  * *_strcat - function that concatenates two strings
+  *
+  * @dest: input1
+  * @src: input2
+  * Return: always 0
+  */
+char *_strcat(char *dest, char *src)
+{
+	char s1[98] = "Hello";
+	char s2[] = "world \n";
+	char *pre;
+
+	if (s1 != '\0')
+		ptr = _strcat(s1, s2);
+	printf("%s", ptr);
+	return (0);
+}
+

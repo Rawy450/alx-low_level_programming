e8a358a5d33554423121143d3e5cd8c3a2be56a4
Rawy450 <Rawy450>
2023-08-28 11:36:33 -0700

    my first commit

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include"main.h"
+#include<stdio.h>
+/**
+  * _memset - function that fills memory with a constant byte
+  * @n: bytes of the memory area pointed
+  * @b: constant
+  * @s: the constant byte b
+  * Return: a pointer to the memory area s
+  */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; n > 0; i++; n--)
+	{
+		s[i] = b;
+	}
+	return (s);
+}

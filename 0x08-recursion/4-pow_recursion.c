6c8f192d9050b8a7a2e5d1340a560e2c17220fd5
Rawy450 <Rawy450>
2023-08-30 12:02:17 -0700

    my first commit

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* _pow_recursion - function that returns the value
+* of x raised to the power of y.
+* @x: inp 1
+* @y: inp 2
+* Return: pointer the byets in 's'
+*/
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (x);
+	}
+	else if (y > 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}

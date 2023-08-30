cd908bde6cd07bf0260f4012ae5d9a046e73fecb
Rawy450 <Rawy450>
2023-08-30 10:37:42 -0700

    my first commit

---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,10 @@
+#include<unistd.h>
+/**
+  * _putchar - vrites the character c
+  * @c: the charcter to print
+  * Return: success 1
+  */
+int _putchar(char c)
+{
+	return (1, &c, 1);
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,6 @@
+#ifndef MYHEADERS_H
+#define MYHEADERS_H
+
+void _puts_recursion(char *s);
+int _putchar(char c);
+#endif

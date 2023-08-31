a7f6818a3be0e8b57dd4545752a527b4eb7df762
Rawy450 <Rawy450>
2023-08-31 15:47:38 -0700

    my first commit

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,46 +1,45 @@
 #include "main.h"
-#include <stdio.h>
 /**
-  * last_index -return the last_index
-  * @s: pointer
-  * Return: int
-  */
-int is_palindrome(char *s)
-int check(char *s, int start, int end, int mod)
-int last_index(char *s)
-{
-	int n = 0;
-
-	if (*s > '\0')
-		n += last_index(s + 1) + 1;
-	return 0;
-}
-
-/**
-  * is_palindrome -  string is a palindrome
-  * @s: input
-  * Return: 0 or 1
-  */
+* is_palindrome - Entry point
+* Description - A function that takes a pointer to an int
+* *@s: the function accepts an input saved into s
+* Return: Nothing for now
+*/
 int is_palindrome(char *s)
 {
-	int end = last_index(s);
-
-	return (check(s, 0, end - 1, end % 2));
-}
-
-/**
-  * check - check of palindrome
-  * @s: string
-  * @start: int move to R
-  * @end: int move to L
-  * Return: 0 or 1
-  */
-int check(char *s, int start, int end, int mod)
-{
-	if (start == end && mod != 0 || start == end + 1 mod == 0)
+	if (*s == 0)
 		return (1);
-	else if (s[start != s[end]])
+	return (check_pal(s, 0, _strlen_recursion(s)));
+}
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string to calculate the length of
+ *
+ * Return: length of the string
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
 		return (0);
-	else
-		return (check(s, start + 1, end - 1, mod));
+	return (1 + _strlen_recursion(s + 1));
+}
+
+/**
+ * check_pal - checks the characters recursively for palindrome
+ * @s: string to check
+ * @i: iterator
+ * @len: length of the string
+ *
+ * Return: 1 if palindrome, 0 if not
+ */
+
+int check_pal(char *s, int i, int len)
+{
+	if (*(s + i) != *(s + len - 1))
+		return (0);
+	if (i >= len)
+		return (1);
+	return (check_pal(s, i + 1, len - 1));
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,16 +1,16 @@
-#ifndef MYHEADERS_H
-#define MYHEADERS_H
+#ifndef MAIN_H
+#define MAIN_H
 
-void _puts_recursion(char *s);
 int _putchar(char c);
+void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
-int square(int n, int val);
 int is_prime_number(int n);
 int is_palindrome(char *s);
-int check(char *s, int start, int end, int mod);
-int last_index(char *s);
-#endif
+int wildcmp(char *s1, char *s2);
+int check_pal(char *s, int i, int len);
+
+#endif /*MAIN_H*/

93a95f23a1c73b8d26a48a6eca9a104511c3da14
Rawy450 <Rawy450>
2023-08-31 13:59:46 -0700

    my first commit

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  * is_palindrome -  string is a palindrome
+  * @s: input
+  * Return: 1 if a string is a palindrome and 0 if not
+  */
+int is_palindrome(char *s)
+{
+	int *s;
+
+	for (*s = 0, *s < str_len(*s) / 2, *s++);
+		if ( a[*s] != a[str_len - 1 - *s])
+		{
+			return (0);
+		}
+}

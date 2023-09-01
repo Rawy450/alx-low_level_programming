142dec04e86f8ad1ecf44a0a75e8c49ec0616d72
Rawy450 <Rawy450>
2023-09-01 09:32:59 -0700

    my first commit

---
+++ 0x09-static_libraries/isupper.c
@@ -0,0 +1,13 @@
+#include"main.h"
+int _isupper(int c)
+{
+	char ch = 'A';
+
+    if (isupper(ch)) {
+        printf("%c is an uppercase letter.\n", ch);
+    } else {
+        printf("%c is not an uppercase letter.\n", ch);
+    }
+
+    return 0;
+}

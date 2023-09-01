c827f8ee25aad70820902fcb790fa29ede6ddf6c
Rawy450 <Rawy450>
2023-09-01 12:04:01 -0700

    my first commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+  * main - emty point
+  * return: 0
+  */
+int main(int argc, char **argv)
+{
+	printf("%d \n", argc);
+	printf("%s \n", argv[argc - 1]);
+	printf("%s \n", argv[0]);
+	return (0);
+}
+

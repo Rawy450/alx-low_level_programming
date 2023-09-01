f0a28c4c5d76c72a9b3da48a2dc9877d4b28a7a5
Rawy450 <Rawy450>
2023-09-01 13:30:12 -0700

    my first commit

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,8 +5,8 @@
   *@argv: input 2
   * return: 0
   */
-int main(int argc, char **argv)
+int main(int argc, char **argv __attribute__((unused)))
 {
-	printf("%d \n", argc);
+	printf("%d \n", argc - 1);
 	return (0);
 }

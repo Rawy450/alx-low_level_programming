2e153676e3fdb615a2fa4994cd33d19f4c7def24
Rawy450 <Rawy450>
2023-09-02 13:21:13 -0700

    my first commit

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,12 +13,12 @@ int main(int argc, char *argv[])
 	while (--argc)
 	{
 		for (c = argv[argc]; *c; c++)
-		{
+		
 			if (*c <'0' || *c > '9')
-				printf("Error\n");
-		return (1);
-		}
-		sum += atoi(argv[argc]);
+		
+		return (printf("Error\n"), 1);
+		
+		sum += (atoi(argv[argc]));
 	}
 	printf("%d\n", sum);
 	return (0);

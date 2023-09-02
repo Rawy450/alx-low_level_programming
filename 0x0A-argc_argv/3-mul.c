0f2e3f73c25f393805186bcb1a1edb1e89b8ab78
Rawy450 <Rawy450>
2023-09-02 02:42:01 -0700

    my first commit

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,11 +9,12 @@ int main(int argc, char *argv[])
 {
 	int sum;
 
-	if (argc !=  2)
+	if (argc !=  3)
 	{
 		printf("Error");
 	return (1);
 	}
+	else
 	{
 		sum = atoi(argv[1]) * atoi(argv[2]);
 		printf("%d\n", sum);

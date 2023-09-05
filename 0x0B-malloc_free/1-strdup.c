1624ba5b1b8f9bb435bdd0c31965eb47c6eb0a5a
Rawy450 <Rawy450>
2023-09-05 10:42:23 -0700

    my first commit

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * _strdup - function that returns a pointer
+  * @size: input
+  * @ str: input
+  * Return: 0 success
+  */
+char *_strdup(char *str)
+{
+	int i = 0, size = 0;
+	char *m;
+		if (str == 0)
+			return (0);
+	for (; str[size] != '\0'; size++)
+		;
+
+		m = malloc(size * str[size] + 1);
+	if (m == 0)
+		return (0);
+
+	else
+	{
+		for (; i < size; i++)
+			;
+			m[i] = str[i];
+	}
+	return (m);
+}

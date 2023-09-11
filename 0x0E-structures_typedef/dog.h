82a752c74e37b749986e0af737c04ea76bf7392c
Rawy450 <Rawy450>
2023-09-11 13:08:20 -0700

    my first commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,11 +17,6 @@ typedef struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
-typedef struct dog_t
-{
-	char *name;
-	char *owner;
-	float age;
-} dog_t;
+typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 #endif

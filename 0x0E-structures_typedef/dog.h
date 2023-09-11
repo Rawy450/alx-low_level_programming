214b089bb45fef1674af0cfc7a7fecab45d96391
Rawy450 <Rawy450>
2023-09-11 10:05:58 -0700

    my first commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+  *  struct dog - function of dog struct
+  * Return: Always 0
+  */
+
+typedef struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+} dog;
+#endif

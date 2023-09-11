9cd86c9910ab123cdd4bd3e3f23c872ec6614bdf
Rawy450 <Rawy450>
2023-09-11 10:35:01 -0700

    my first commit

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include"dog.h"
+/**
+  * init_dog - function of dog struct
+  * @name: input
+  * @owner: input2
+  * @age: input3
+  * @d: inp
+  * Return: Always 0
+  */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+	d->age = age;
+	d->owner = owner;
+	}
+}

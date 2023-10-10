929081843100336b848f42ac4f89e962735c688a
Rawy450 <Rawy450>
2023-10-10 04:15:54 -0700

    my first commit

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * flip_bits - Returns the number of bits needed to fli
+ * from one number to another.
+ * @n: First number.
+ * @m: Second number.
+ * Return: Number of bits to flip.
+ */
+unsigned int flip_bits(unsigned int n, unsigned int m)
+{
+	unsigned int a = n ^ m;
+	unsigned int f = 0;
+
+	while (a > 0)
+	{
+		f += d & 1;
+		a >>= 1;
+	}
+
+	return (f);
+}

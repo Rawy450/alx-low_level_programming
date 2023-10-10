57edaa31694573d9ac573ea2dc672f56611317f7
Rawy450 <Rawy450>
2023-10-10 10:34:22 -0700

    my first commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,43 +4,38 @@
 #include <unistd.h>
 #include"main.h"
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output
- * @filename: the name of the file to read
- * @letters: the number of letters it should read and print
- *
- * Return: the actual number of letters it could read and print
- * if the file can not be opened or read, return 0
- * if filename is NULL return 0
- * if write fails or does not write the expected amount of bytes, return 0
+ * create_file - Creates a file with specified content.
+ * @filename: The name of the file to create.
+ * @text_content: The content to write to the file.
+ * Return: 1 on success, -1 on failure.
  */
-ssize_t read_textfile(const char *filename, size_t letters)
+
+int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	ssize_t n, written;
-	char buffer[1024];
+	int c, v = 0, write_len;
 
 	if (filename == NULL)
-		return (0);
+		return (-1);
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (0);
+	c = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
-	n = read(fd, buffer, letters);
-	if (n == -1)
+	if (c == -1)
+		return (-1);
+
+	if (text_content != NULL)
 	{
-		close(fd);
-		return (0);
-	}
+		while (text_content[v])
+			v++;
 
-	buffer[n] = '\0';
-	written = write(1, buffer, n);
-	if (written != n)
-	{
-		close(fd);
-		return (0);
+		write_len = write(c, text_content, v);
+
+		if (write_len != v)
+		{
+			close(c);
+			return (-1);
+		}
 	}
 
 	close(fd);
-	return (n);
+	return (1);
 }

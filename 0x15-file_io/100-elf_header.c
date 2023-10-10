baba62f679c37eb6cf8389f4ffdb571f4dfafebb
Rawy450 <Rawy450>
2023-10-10 12:01:27 -0700

    my first commit

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,47 +1,51 @@
 #include <stdio.h>
-#include <sys/types.h>
-#include <sys/stat.h>
+#include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <elf.h>
-/**
- * print_elf_header - Prints the ELF header of a file.
- * @filename: The name of the file to print the ELF header of.
- */
-void print_elf_header(const char *filename)
-{
-	int fd;
-	Elf64_Ehdr ehdr;
-	fd = open(filename, O_RDONLY);
-	if (fd == -1) {
-		perror("open");
-		return;
-	}
-	if (read(fd, &ehdr, sizeof(ehdr)) != sizeof(ehdr)) {
-		perror("read");
-		close(fd);
-		return;
-	}
-	close(fd);
-	printf("ELF header:\n");
-	printf("  Magic:               %02x %02x %02x %02x\n",
-	       ehdr.e_ident[0], ehdr.e_ident[1],
-	       ehdr.e_ident[2], ehdr.e_ident[3]);
-	printf("  Class:               %d\n", ehdr.e_ident[4]);
-	printf("  Data:                %d\n", ehdr.e_ident[5]);
-	printf("  Version:             %d\n", ehdr.e_ident[6]);
-	printf("  OS/ABI:              %d\n", ehdr.e_ident[7]);
-	printf("  ABI Version:         %d\n", ehdr.e_ident[8]);
-	printf("  Type:                %d\n", ehdr.e_type);
-	printf("  Entry point address:  0x%08x\n", ehdr.e_entry);
+
+void display_error(const char* message) {
+  fprintf(stderr, "%s\n", message);
+  exit(98);
 }
-/**
- * main - Prints the ELF header of a file.
- * @argc: The number of arguments.
- * @argv: The arguments.
- *
- * Return: 0 on success, 1 on failure.
- */
-int main(int argc, char *argv[])
-{
-	if (argc)
+
+void display_elf_header(const char* filename) {
+  int fd = open(filename, O_RDONLY);
+  if (fd == -1) {
+  display_error("Failed to open the file");
+  }
+
+  Elf64_Ehdr elf_header;
+  if (read(fd, &elf_header, sizeof(Elf64_Ehdr)) != sizeof(Elf64_Ehdr)) {
+  display_error("Failed to read the ELF header");
+  }
+
+  // Check if it's a valid ELF file
+  if (elf_header.e_ident[EI_MAG0] != ELFMAG0 || elf_header.e_ident[EI_MAG1] != ELFMAG1 ||
+  elf_header.e_ident[EI_MAG2] != ELFMAG2 || elf_header.e_ident[EI_MAG3] != ELFMAG3) {
+  display_error("Not a valid ELF file");
+  }
+
+  // Display the required information
+  printf("Magic: %02x %02x %02x %02x\n", elf_header.e_ident[EI_MAG0], elf_header.e_ident[EI_MAG1],
+  elf_header.e_ident[EI_MAG2], elf_header.e_ident[EI_MAG3]);
+  printf("Class: %d\n", elf_header.e_ident[EI_CLASS]);
+  printf("Data: %d\n", elf_header.e_ident[EI_DATA]);
+  printf("Version: %d\n", elf_header.e_ident[EI_VERSION]);
+  printf("OS/ABI: %d\n", elf_header.e_ident[EI_OSABI]);
+  printf("ABI Version: %d\n", elf_header.e_ident[EI_ABIVERSION]);
+  printf("Type: %d\n", elf_header.e_type);
+  printf("Entry point address: %lx\n", elf_header.e_entry);
+
+  close(fd);
+}
+
+int main(int argc, char* argv[]) {
+  if (argc != 2) {
+  display_error("Usage: elf_header elf_filename");
+  }
+
+  display_elf_header(argv[1]);
+
+  return 0;
+}
